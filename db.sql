4b33a01781ce8e5edb646ba97f4f197c08369446
turgay2317 <90829101+turgay2317@users.noreply.github.com>
2021-12-30 04:19:10 +0300

    Database updated

--- db.sql
+++ db.sql
@@ -1,12 +1,14 @@
-/*
--- Query: SELECT * FROM users
-LIMIT 0, 1000
+CREATE DATABASE `testdb`;
 
--- Date: 2021-12-30 04:14
-*/
-INSERT INTO `` (`userID`,`userName`) VALUES (1,'Turgay Ceylan');
-INSERT INTO `` (`userID`,`userName`) VALUES (2,'John Doe');
-INSERT INTO `` (`userID`,`userName`) VALUES (3,'Peter Parker');
-INSERT INTO `` (`userID`,`userName`) VALUES (4,'Ben Parker');
-INSERT INTO `` (`userID`,`userName`) VALUES (300,'Serol Teber');
-INSERT INTO `` (`userID`,`userName`) VALUES (309,'Fyodor Dostoyevski');
+CREATE TABLE `users` (
+  `userID` int NOT NULL AUTO_INCREMENT,
+  `userName` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`userID`)
+) ENGINE=InnoDB AUTO_INCREMENT=400 DEFAULT CHARSET=utf8mb3;
+
+INSERT INTO `users` (`userID`,`userName`) VALUES (1,'Turgay Ceylan');
+INSERT INTO `users` (`userID`,`userName`) VALUES (2,'John Doe');
+INSERT INTO `users` (`userID`,`userName`) VALUES (3,'Peter Parker');
+INSERT INTO `users` (`userID`,`userName`) VALUES (4,'Ben Parker');
+INSERT INTO `users` (`userID`,`userName`) VALUES (300,'Serol Teber');
+INSERT INTO `users` (`userID`,`userName`) VALUES (309,'Fyodor Dostoyevski');
