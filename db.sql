47a847ad472aa9daaad4e81ea9eb5760d5a58d70
turgay2317 <90829101+turgay2317@users.noreply.github.com>
2021-12-30 04:15:05 +0300

    Database uploaded

---
+++ db.sql
@@ -0,0 +1,12 @@
+/*
+-- Query: SELECT * FROM users
+LIMIT 0, 1000
+
+-- Date: 2021-12-30 04:14
+*/
+INSERT INTO `` (`userID`,`userName`) VALUES (1,'Turgay Ceylan');
+INSERT INTO `` (`userID`,`userName`) VALUES (2,'John Doe');
+INSERT INTO `` (`userID`,`userName`) VALUES (3,'Peter Parker');
+INSERT INTO `` (`userID`,`userName`) VALUES (4,'Ben Parker');
+INSERT INTO `` (`userID`,`userName`) VALUES (300,'Serol Teber');
+INSERT INTO `` (`userID`,`userName`) VALUES (309,'Fyodor Dostoyevski');
